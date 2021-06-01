d835a0a3e15ffebd9eebbef040a98e88d10ae193
Programmer-Shuvam <shuvamanupam7@gmail.com>
2021-06-01 20:13:08 +0530

    kuch bhi nahi

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -9,16 +9,16 @@
 
 // spit out all the keys/values from the form in HTML for email
 // uses an array of keys if provided or the object to determine field order
-let forms = document.querySelectorAll('.php-email-form');
-forms.onsubmit = function(e){
-    event.preventDefault();
-    var xhttp = new XMLHttpRequest();
-    // xhttp.onreadystatechange = function() {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //     document.getElementById("demo").innerHTML = this.responseText;
-    //     }
-    // };
-    xhttp.open("POST", "https://script.google.com/macros/s/AKfycbyj76RooElV9hASL4y6lZUeSjMzl6ylogER56tBx5Gs6VWEz1g-wLCiPvU_C__UB2OoXQ/exec", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("name=shuvam&email=shuvamanupam7@gmail.com&subject=appreciation&message=thank_you");
-};
+// let forms = document.querySelectorAll('.php-email-form');
+// forms.onsubmit = function(e){
+//     event.preventDefault();
+//     var xhttp = new XMLHttpRequest();
+//     // xhttp.onreadystatechange = function() {
+//     //     if (this.readyState == 4 && this.status == 200) {
+//     //     document.getElementById("demo").innerHTML = this.responseText;
+//     //     }
+//     // };
+//     xhttp.open("POST", "https://script.google.com/macros/s/AKfycbyj76RooElV9hASL4y6lZUeSjMzl6ylogER56tBx5Gs6VWEz1g-wLCiPvU_C__UB2OoXQ/exec", true);
+//     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//     xhttp.send("name=shuvam&email=shuvamanupam7@gmail.com&subject=appreciation&message=thank_you");
+// };
